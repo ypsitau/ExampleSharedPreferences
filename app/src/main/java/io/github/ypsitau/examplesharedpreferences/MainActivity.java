3260914b624e23246803235a1209156d1fb2e22c
ypsitau <ypsitau@nifty.com>
2018-05-24 11:55:43 +0900

    added TextView that indicates a current value of SeekBar

--- app/src/main/java/io/github/ypsitau/examplesharedpreferences/MainActivity.java
+++ app/src/main/java/io/github/ypsitau/examplesharedpreferences/MainActivity.java
@@ -1,13 +1,37 @@
 package io.github.ypsitau.examplesharedpreferences;
 
 import android.content.SharedPreferences;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+	private class OnSeekBarChangeListener implements SeekBar.OnSeekBarChangeListener {
+		TextView textViewAttached;
+		OnSeekBarChangeListener(TextView textViewAttached) {
+			this.textViewAttached = textViewAttached;
+		}
+
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+			textViewAttached.setText(String.valueOf(progress));
+		}
+
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {
+			// nothing to do
+		}
+
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			// nothing to do
+		}
+	}
+
 	static final String KEY_EDITTEXT1 = "editText1";
 	static final String KEY_EDITTEXT2 = "editText2";
 	static final String KEY_EDITTEXT3 = "editText3";
@@ -26,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
 	private SeekBar seekBar1;
 	private SeekBar seekBar2;
 	private SeekBar seekBar3;
+	private TextView textView_progress1;
+	private TextView textView_progress2;
+	private TextView textView_progress3;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,11 +67,17 @@ public class MainActivity extends AppCompatActivity {
 		seekBar1 = findViewById(R.id.seekBar1);
 		seekBar2 = findViewById(R.id.seekBar2);
 		seekBar3 = findViewById(R.id.seekBar3);
+		textView_progress1 = findViewById(R.id.textView_progress1);
+		textView_progress2 = findViewById(R.id.textView_progress2);
+		textView_progress3 = findViewById(R.id.textView_progress3);
+		seekBar1.setOnSeekBarChangeListener(new OnSeekBarChangeListener(textView_progress1));
+		seekBar2.setOnSeekBarChangeListener(new OnSeekBarChangeListener(textView_progress2));
+		seekBar3.setOnSeekBarChangeListener(new OnSeekBarChangeListener(textView_progress3));
 	}
 
 	@Override
-	protected void onStart() {
-		super.onStart();
+	protected void onResume() {
+		super.onResume();
 		SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
 		editText1.setText(sharedPref.getString(KEY_EDITTEXT1, "value1"));
 		editText2.setText(sharedPref.getString(KEY_EDITTEXT2, "value2"));
@@ -56,9 +90,11 @@ public class MainActivity extends AppCompatActivity {
 		seekBar3.setProgress(sharedPref.getInt(KEY_SEEKBAR3, 50));
 	}
 
+	// https://developer.android.com/reference/android/app/Activity
+	// You should use the onPause() method to write any persistent data.
 	@Override
-	protected void onStop() {
-		super.onStop();
+	protected void onPause() {
+		super.onPause();
 		SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
 		SharedPreferences.Editor editor = sharedPref.edit();
 		editor.putString(KEY_EDITTEXT1, editText1.getText().toString());
